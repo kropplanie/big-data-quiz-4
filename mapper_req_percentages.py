996e3e0c5c6224b2065feeb3e26dd5622d5efd9c
kropplanie <kropplanie@gmail.com>
2024-10-13 15:19:34 -0400

    Update mapper_req_percentages.py

--- mapper_req_percentages.py
+++ mapper_req_percentages.py
@@ -1,24 +1,19 @@
 #!/usr/bin/env python
-"""mapper_req_percentage.py"""
-
 import sys, re
+import random
 
 def main(argv):
-  # we are going to use a regex method to find the request type in the row
-  line = sys.stdin.readline()
-  pattern = re.compile(r'\"(\w+)\s')  # this is the pattern to capture the method inside quotes
-  
-  try:
-      while line:
-          for request_type in pattern.findall(line):
-              print("reporter:counter:LogMetrics,TotalRecords,1")
-              print '%s\t%s' % (request_type, 1)
-          line = sys.stdin.readline()
-  except EOFError as error:
-      return None
+    line = sys.stdin.readline()
+    pattern = re.compile(r'\"(\w+)\s')
+    try:
+        while line:
+            for req_type in pattern.findall(line):
+                print (req_type.lower() + "\t" + "1")
+            line = sys.stdin.readline()
+    except EOFError as error:
+        return None
 
 if __name__ == "__main__":
     main(sys.argv)
-
 
 
