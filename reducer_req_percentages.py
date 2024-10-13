f0d0ff6720beedb6cc1097f10785e4e58918fd95
kropplanie <kropplanie@gmail.com>
2024-10-13 17:43:52 -0400

    Update reducer_req_percentages.py

--- reducer_req_percentages.py
+++ reducer_req_percentages.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 """reducer.py"""
 
+from operator import itemgetter
 import sys
 
 current_word = None
 current_count = 0
 word = None
-total_count = 0
-word_counts = {}
 
 # input comes from STDIN
 for line in sys.stdin:
@@ -17,38 +16,29 @@ for line in sys.stdin:
     # parse the input we got from mapper.py
     word, count = line.split('\t', 1)
 
-    # Convert count (currently a string) to int
+    # convert count (currently a string) to int
     try:
         count = int(count)
     except ValueError:
-        # Count was not a number, so silently ignore/discard this line
+        # count was not a number, so silently
+        # ignore/discard this line
         continue
 
-    # Check if this line contains the special total count
-    if word == "__TOTAL__":
+    if word == '__TOTAL__':
         total_count += count
         continue
-
-    # This IF-switch only works because Hadoop sorts map output
+    
+    # this IF-switch only works because Hadoop sorts map output
     # by key (here: word) before it is passed to the reducer
     if current_word == word:
         current_count += count
     else:
         if current_word:
-            # Store the count for the current word
-            word_counts[current_word] = current_count
-
+            # write result to STDOUT
+            print ('%s\t%s' % (current_word, current_count))
         current_count = count
         current_word = word
 
-# Do not forget to output the last word if needed!
+# do not forget to output the last word if needed!
 if current_word == word:
-    word_counts[current_word] = current_count
-
-# Calculate and print the percentage for each word
-if total_count > 0:
-    for word, count in word_counts.items():
-        percentage = (count / total_count) * 100
-        print(f'{word}\t{percentage:.2f}%')
-else:
-    print("Error: Total count is zero or not found.")
+    print ('%s\t%s' % (current_word, current_count))
