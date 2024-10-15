b2bdba7c372b0fa36a928215ae955512b2434982
kropplanie <kropplanie@gmail.com>
2024-10-15 13:14:40 -0400

    Update reducer_server_top5.py

--- reducer_server_top5.py
+++ reducer_server_top5.py
@@ -2,7 +2,6 @@
 import sys
 import heapq
 
-# Assuming the total count is passed as an environment variable or command-line argument
 current_word = None
 current_count = 0
 word = None
@@ -32,12 +31,12 @@ for line in sys.stdin:
     else:
         if current_word:
             
-            # Add to heap if it's in top N or replace the smallest if necessary
+            # add to heap if it's in top N or replace the smallest if necessary
             if len(top_n_heap) < top_n:
                 heapq.heappush(top_n_heap, (current_count, current_word))
             else:
-                # Check if we should replace the smallest
-                if current_count > top_n_heap[0][0]:  # Compare with the smallest count in the heap
+                # check if we should replace the smallest
+                if current_count > top_n_heap[0][0]: 
                     heapq.heappushpop(top_n_heap, (current_count, current_word))
         current_count = count
         current_word = word
@@ -50,7 +49,7 @@ if current_word == word:
         if current_count > top_n_heap[0][0]:  # Compare with the smallest count in the heap
             heapq.heappushpop(top_n_heap, (current_count, current_word))
             
-# Output the top N elements sorted by count in descending order
+# peint the top N elements sorted by count in descending order
 top_elements = sorted(top_n_heap, key=lambda x: x[0], reverse=True)
 for count, word in top_elements:
     print(f"{word}\t{count}")
