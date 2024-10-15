a6b89a97470f0f9350e58c7807f396fa81fbfd6e
kropplanie <kropplanie@gmail.com>
2024-10-15 12:51:32 -0400

    Update mapper_response.py

--- mapper_response.py
+++ mapper_response.py
@@ -4,20 +4,20 @@ import re
 
 def main(argv):
     total_count = 0
-    # Regular expression to match the HTTP response code
+    # regular expression to match the esponse code
     pattern = re.compile(r'"\s*(\d{3})\s')
 
-    # Read lines from standard input
+    # read lines from standard input
     for line in sys.stdin:
-        # Find all response codes in the line
+        # find all response codes in the line
         response_codes = pattern.findall(line)
         
-        # Increment the counter for each response code
+        
         for response in response_codes:
             # Convert response code to integer for comparison
             response_code = int(response)
             
-            # Map to correct category
+            # map to correct category
             if response_code < 200:
                 print(f"{'informational_responses'}\t1")
             elif response_code < 300:
@@ -34,9 +34,9 @@ def main(argv):
             # Use Hadoop's counter mechanism by writing to stderr
             # The format is: 
             #    <counter group name> <counter name> <increment>
-            sys.stderr.write(f"reporter:counter:ResponseCodes,{response},1\n")
+            #sys.stderr.write(f"reporter:counter:ResponseCodes,{response},1\n")
             
-    # Emit the total count as a special key-value pair
+    # emit the total count as a key-value pair
     print(f"__TOTAL__\t{total_count}")
 
 if __name__ == "__main__":
